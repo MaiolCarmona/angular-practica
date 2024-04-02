6582081aaf22dddcedc73fa50c04040e3d034b75
Maiol Carmona <maiolcarmona@gmail.com>
2024-04-02 21:33:22 +0200

    carga dinamica de baners en home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { Banner } from 'src/app/common/interfaces/banner'
+import { HomeService } from 'src/app/services/home.service'
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  panoBanners: Banner[] = []
+  regularBanners: Banner[] = []
+  constructor (private readonly homeService: HomeService) {
+    this.loadPanoBanners()
+    this.loadRegularBanners()
+  }
 
+  loadPanoBanners (): void {
+    this.homeService.getPanoBanners().subscribe(
+      {
+        next: banners => {
+          this.panoBanners = banners
+        },
+        error: error => {
+          console.error(error)
+        }
+      }
+    )
+  }
+
+  loadRegularBanners (): void {
+    this.homeService.getRegularBanners().subscribe(
+      {
+        next: bann => {
+          console.log(bann)
+          this.regularBanners = bann
+        },
+        error: error => {
+          console.error(error)
+        }
+      }
+    )
+  }
 }
