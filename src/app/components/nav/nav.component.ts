0e0b50f7bcf58bb6e45fde37d2f83cac11a9e1de
Maiol Carmona <maiolcarmona@gmail.com>
2024-04-04 18:03:27 +0200

    terminado el navbar

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core'
+import { IconDefinition } from '@fortawesome/free-brands-svg-icons'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+import { faMagnifyingGlass, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import { Category } from 'src/app/common/interfaces/category'
+import { ShopService } from 'src/app/services/shop.service'
 
 @Component({
   selector: 'app-nav',
@@ -6,5 +11,24 @@ import { Component } from '@angular/core'
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent {
+  glass: IconDefinition = faMagnifyingGlass
+  user: IconDefinition = faUser
+  cart: IconDefinition = faShoppingCart
 
+  categories: Category[] = []
+
+  constructor (private readonly shopService: ShopService) {
+    this.loadCategories()
+  }
+
+  private loadCategories (): void {
+    this.shopService.getCategories().subscribe({
+      next: data => {
+        this.categories = data
+      },
+      error: (err) => {
+        console.error(err)
+      }
+    })
+  }
 }
